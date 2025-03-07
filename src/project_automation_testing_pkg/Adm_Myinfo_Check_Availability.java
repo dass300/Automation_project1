5be14b0aca69c35b8fc62eaec27cdc0cd62ef340
dass300 <dass300@gmail.com>
2025-03-07 12:23:51 +0530

    Adm my info check availability added

--- src/project_automation_testing_pkg/Adm_Myinfo_Check_Availability.java
+++ src/project_automation_testing_pkg/Adm_Myinfo_Check_Availability.java
@@ -1,8 +1,7 @@
 package project_automation_testing_pkg;
 
 import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+import java.awt.RenderingHints.Key;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -26,107 +25,123 @@ public class Adm_Myinfo_Check_Availability {
 		Adm_LoginPage admLoginPage = new Adm_LoginPage(driver);
 		admLoginPage.login("Admin", "admin123");
 
-		// locate PIM element
+		/// *******///
 
-		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a")).click();
+		// locate Myinfo tab
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[6]/a")).click();
 
-		// locate Add button
-		driver.findElement(By.xpath("//button[text()=' Add ']")).click();
+		// locate Personal Details tab
+		driver.findElement(By.linkText("Personal Details")).click();
+		Thread.sleep(1000);
 
-		
-		// locate uploadButton
+		// update Personal Details
 
-		WebElement uploadButton = driver.findElement(By.xpath("//i[@class='oxd-icon bi-plus']"));
-		uploadButton.click();
-
-		WebElement uploadPic = driver.findElement(By.xpath("//input[@type='file']"));
-		uploadPic.sendKeys("C:\\Users\\dazzv\\OneDrive\\Desktop\\PRJ_AUTM\\asset\\propic1.jpeg");
-		Thread.sleep(2000);
-
-		// for closing upload window
-		Robot robot = new Robot();
-		robot.keyPress(KeyEvent.VK_ESCAPE);
-		robot.keyRelease(KeyEvent.VK_ESCAPE);
-		Thread.sleep(2000);
-
-		// Locate the input field
-		driver.findElement(By.name("firstName")).sendKeys("popo");
-		driver.findElement(By.name("middleName")).sendKeys("pp");
-		driver.findElement(By.name("lastName")).sendKeys("lulu");
-
-		Thread.sleep(2000);
-
-		// Locate the input field
-		WebElement inputField = driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/input"));
-
-		// Clear the input field by selecting all and then deleting
-		inputField.sendKeys(Keys.CONTROL + "a");
-		inputField.sendKeys(Keys.DELETE);
-
+		// locate Driver License Number input field
+		WebElement license = driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[1]/div/div[2]/input"));
+		license.sendKeys(Keys.CONTROL + "a");
+		license.sendKeys(Keys.DELETE);
+		Thread.sleep(1000);
 		// Enter new data
-		inputField.sendKeys("1001");
-
-		// Submit the form
-		driver.findElement(By.xpath("//button[@type='submit']")).submit();
-
+		license.sendKeys("DL-10-2025-1234567");
 		Thread.sleep(1000);
 
-		// Locate the otherID-input field
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[1]/div[2]/div/div[2]/input"))
-				.sendKeys("OTH-123456");
+		// save the changes
+		driver.findElement(By.xpath("//button[text()=' Save ']")).click();
+		Thread.sleep(1000);
+		/// *******///
 
-		// Locate Driver's License Number-input field
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[1]/div/div[2]/input"))
-				.sendKeys("DV123456");
-
-		// Locate License Expiry Date-input field
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[2]/div[2]/div[2]/div/div[2]/div/div/input"))
-				.sendKeys("2025-12-02");
-
-		// Locate Nationality-input field
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[1]/div/div[2]/div/div"))
-				.click();
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[1]/div/div[2]/div/div[2]/div[4]/span"))
-				.click();
+		// locate Contact Details tab
+		driver.findElement(By.linkText("Contact Details")).click();
 		Thread.sleep(1000);
 
-		// Locate Marital Status-input field
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[2]/div/div[2]/div/div"))
-				.click();
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/div[2]/div/div[2]/div/div[2]/div[2]"))
-				.click();
+		// update Contact Details Details
+
+		// locate Work Email -input field
+		WebElement email = driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div/div[1]/div/div[2]/input"));
+		email.sendKeys(Keys.CONTROL + "a");
+		email.sendKeys(Keys.DELETE);
+		Thread.sleep(1000);
+		// Enter new data
+		email.sendKeys("demo@gmail.com");
 		Thread.sleep(1000);
 
-		// Locate Date of Birth-input field
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[1]/div/div[2]/div/div/input"))
-				.sendKeys("2000-10-02");
-
+		// save the changes
+		driver.findElement(By.xpath("//button[text()=' Save ']")).click();
 		Thread.sleep(1000);
+		/// *******///
 
-		// Locate Gender-input field
+		// locate Job tab
+		driver.findElement(By.linkText("Job")).click();
 
-		driver.findElement(By.xpath(
-				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[2]/div[2]/div/div[2]/div[2]/div[2]/div/label/span"))
-				.click();
+		// update Job Details
 
-		// Locate and click the save button
+		// cannot update any details
+
+		// locate Salary tab
+		driver.findElement(By.linkText("Salary")).click();
+		// update Contact Details Details
+
+		// cannot update any details
+
+		// locate Qualifications tab
+		driver.findElement(By.linkText("Qualifications")).click();
+		// update Contact Details Details
+
+		// Locate the Education add button
 		driver.findElement(
-				By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[4]/button"))
-				.submit();
 
+				By.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/div/button")).click();
+
+		// Locate the Level select field
+		driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[1]/div/div[1]/div/div[2]/div/div[1]/div[2]/i"))
+				.click();
+
+		// Locate the option for Level select field
+		driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[1]/div/div[1]/div/div[2]/div/div[2]/div[2]/span"))
+				.click();
+
+		// Locate the Institute-input field
+		driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[1]/div/div[2]/div/div[2]/input"))
+				.sendKeys("ABC coleage of Engineering");
+
+		// Locate the Major/Specialization -input field
+		driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[1]/div/div[3]/div/div[2]/input"))
+				.sendKeys("Computer science");
+
+		// Locate the Year -input field
+		driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[1]/div/div[4]/div/div[2]/input"))
+				.sendKeys("2019");
+		// Locate the GPA/Score -input field
+		driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[1]/div/div[5]/div/div[2]/input"))
+				.sendKeys("80%");
+
+		// Locate the Start Date -input field
+		driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[2]/div/div[1]/div/div[2]/div/div[1]/input"))
+				.sendKeys("2015-06-06");
+
+		// Locate the End Date -input field
+		driver.findElement(By.xpath(
+				"//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[2]/div/div[2]/div/div[2]/div/div/input"))
+				.sendKeys("2019-03-03");
+
+		// Submit the save button
+		driver.findElement(By
+				.xpath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[3]/div[1]/form/div[3]/button[2]"))
+				.click();
 		Thread.sleep(1000);
+		//// *****/////
 
 		// Close the browser window
-		driver.quit();
+		 driver.quit();
 
 	}
 }
